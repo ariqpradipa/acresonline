39b6c6fa6c97fb196e61b26812ff5b6c651b6722
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-08-03 14:42:20 +0700

    add image and level gauge

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
       </Head>
       <Component {...pageProps} />;
 
--- pages/w/home.js
+++ pages/w/home.js
@@ -1,12 +1,13 @@
 import * as React from "react";
+import Image from "next/image"
 import Cookies from "js-cookie";
 import Button from '@mui/material/Button';
-import axios from "axios";
 
 //icons
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 import CheckroomIcon from '@mui/icons-material/Checkroom';
+import IronIcon from '@mui/icons-material/Iron';
 
 const Swal = require('sweetalert2');
 
@@ -15,9 +16,9 @@ export default function Home() {
 
     const [token, setToken] = React.useState(null);
     const [username, setUsername] = React.useState(null);
+    const percentage = "100%"
 
     React.useEffect(() => {
-
         setToken(Cookies.get("tokenAcres"));
         setUsername(Cookies.get("usernameAcres"));
 
@@ -30,20 +31,27 @@ export default function Home() {
                 <h1>token {token}</h1>
                 <h1>username {username}</h1>
             </div>
+
+            <div className="flex flex-col mb-4 justify-center items-center">
+                <div className="w-[98%] bg-gray-200 h-5 mb-2">
+                    <div className="bg-blue-600 h-5 text-white text-center" style={{width: percentage}}>{percentage}</div>
+                </div>
+                <img src="https://mdbootstrap.com/img/new/standard/city/047.jpg" class="max-w-[90%] h-auto rounded-lg" alt="" />
+            </div>
             <div className="flex flex-col justify-center items-center space-y-2">
                 <div className="flex space-x-2">
-                    <Button variant="outlined" startIcon={<LocalMallIcon />} sx={{ width: 185 }} className="justify-start">
+                    <Button variant="outlined" startIcon={<LocalMallIcon />} className="justify-start w-[45vw]">
                         Store
                     </Button>
-                    <Button variant="outlined" startIcon={<LocalHospitalIcon />} sx={{ width: 185 }} className="justify-start">
+                    <Button variant="outlined" startIcon={<LocalHospitalIcon />} className="justify-start w-[45vw]">
                         Infirmary
                     </Button>
                 </div>
                 <div className="flex space-x-2">
-                    <Button variant="outlined" startIcon={<LocalMallIcon />} sx={{ width: 185 }} className="justify-start">
+                    <Button variant="outlined" startIcon={<IronIcon />} className="justify-start w-[45vw]">
                         Blacksmith
                     </Button>
-                    <Button variant="outlined" startIcon={<CheckroomIcon />} sx={{ width: 185 }} className="justify-start">
+                    <Button variant="outlined" startIcon={<CheckroomIcon />} className="justify-start w-[45vw]">
                         Tailor
                     </Button>
                 </div>
